dc9f5493ee76293d1cf60776df03badeebe1e504
asettar <achrafsettar8@gmail.com>
2026-02-03 19:44:13 +0100

    add programs  migration

---
+++ database/migrations/2026_02_03_183701_create_programs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('programs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('route_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('segment_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('bus_id')->constrained()->cascadeOnDelete();
+            $table->integer('distance');
+            $table->float('price');
+            $table->dateTime('start_time');
+            $table->dateTime('end_time');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('programs');
+    }
+};
