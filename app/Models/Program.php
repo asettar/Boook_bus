73f94c80cbfa5abfde5c1e53448d17f428242b42
asettar <achrafsettar8@gmail.com>
2026-02-04 18:35:34 +0100

    add Program and User models

--- app/Models/Program.php
+++ app/Models/Program.php
@@ -9,4 +9,22 @@ class Program extends Model
 {
     /** @use HasFactory<\Database\Factories\ProgramFactory> */
     use HasFactory;
+
+    protected $fillable = ['route_id', 'segment_id', 'bus_id', 'distance', 'price', 'start_time', 'end_time'];
+
+    public function route() {
+        return $this->belongsTo(Route::class);
+    }
+
+    public function segment() {
+        return $this->belongsTo(Segment::class);
+    }
+
+    public function bus() {
+        return $this->belongsTo(Bus::class);
+    }
+
+    public function reservations() {
+        return $this->hasMany(Reservation::class);
+    }
 }
