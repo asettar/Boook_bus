1927227bab205878e9f78ffac5700f9ae11443f3
asettar <achrafsettar8@gmail.com>
2026-02-04 13:16:28 +0100

    add segment factory

--- database/factories/SegmentFactory.php
+++ database/factories/SegmentFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\City;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +18,8 @@ class SegmentFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'start_city_id' => City::factory(), 
+            'end_city_id' => City::factory(), 
         ];
     }
 }
