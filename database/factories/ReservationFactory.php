13062ee74f8c1dd249ed1f93684cd32ce00a4af7
asettar <achrafsettar8@gmail.com>
2026-02-04 13:24:56 +0100

    add reservation factory

--- database/factories/ReservationFactory.php
+++ database/factories/ReservationFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Models\Program;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +19,10 @@ class ReservationFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'user_id' => User::factory(),
+            'program_id' => Program::factory(),
+            'status' => fake()->randomElement(['pending','confirmed','cancelled']),
+            'seat_number' => fake()->numberBetween(1, 65)
         ];
     }
 }
