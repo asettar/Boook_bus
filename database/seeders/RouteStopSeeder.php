92d86fe5381ce4823a2606fc09ce1ae7da6c65c8
asettar <achrafsettar8@gmail.com>
2026-02-04 15:17:51 +0100

    add routeStop seeder

--- database/seeders/RouteStopSeeder.php
+++ database/seeders/RouteStopSeeder.php
@@ -2,6 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Route;
+use App\Models\RouteStop;
+use App\Models\Station;
+use Database\Factories\RouteStopFactory;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +16,16 @@ class RouteStopSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $routes = Route::all();
+        $stations = Station::all();
+        foreach($routes as $route) {
+            $randomStations = $stations->shuffle()->take(rand(1, 3));
+            foreach($randomStations as $idx => $station) 
+                RouteStop::factory()->create([
+                    'route_id' => $route->id,
+                    'station_id' => $station->id,
+                    'order' => $idx + 1
+            ]);
+        }
     }
 }
