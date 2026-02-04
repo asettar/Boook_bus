acefa4dc675164ec5d509920f509d664bca80dc1
asettar <achrafsettar8@gmail.com>
2026-02-04 13:23:22 +0100

    add program factory

--- database/factories/ProgramFactory.php
+++ database/factories/ProgramFactory.php
@@ -2,6 +2,9 @@
 
 namespace Database\Factories;
 
+use App\Models\Bus;
+use App\Models\Route;
+use App\Models\Segment;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +20,12 @@ class ProgramFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'route_id' => Route::factory(),
+            'segment_id' => Segment::factory(),
+            'bus_id' => Bus::factory(),
+            'distance' => fake()->numberBetween(20, 500),
+            'price' => fake()->randomFloat(2, 30, 500),
+            // startTime, endTime will be manually added 
         ];
     }
 }
